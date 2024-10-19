952598987f84c4f063b8456d0075c79adceff55b
Laiba67897 <154372694+Laiba67897@users.noreply.github.com>
2024-10-19 13:19:23 +0500

    Add files via upload

---
+++ associative array.php
@@ -0,0 +1,53 @@
+ <?php
+
+ $city = [
+    "Pkistan" => "Karachi",
+    "Bangladesh" => "Dhoka",
+    "Japan" => "Tokyo",
+    "Germany" => "Berlin",
+ ];
+
+
+//  METHODS//////
+
+// array_pop($city);
+// array_shift($city);
+// $keys = array_keys($city);
+// $values = array_values($city);
+// $flippedArray = array_flip($city);
+// $reverseArray = array_reverse($city);
+
+
+// pop/// or shift///
+// foreach($city as $el){
+//     echo $el ;
+//  }
+
+
+
+// array keys print////
+// $keys = array_keys($city); ...method
+// foreach($keys as $key){
+//     echo $key;
+//  }
+
+
+ // $values = array_values($city);...mthod
+//  foreach($city as $values){
+//     echo $values;
+//  }
+ 
+// arrayflip/////
+// $flippedArray = array_flip($city);...method
+// foreach($flippedArray as $key => $values){
+//     echo "$values .<br>";
+    
+// }
+
+
+// FLIP REVERSE//////
+// $reverseArray = array_reverse($city); ...method
+// foreach($reverseArray as $key => $values){
+//     echo "$values .<br>";
+    
+// }
